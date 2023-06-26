ea1e9f051f3af12ff7bcb043c242a35c6ed8cbe2
hahak <skekdnl12@gmail.com>
2023-06-26 23:30:24 +0900

    환경 세팅 및 tika추가

---
+++ exam/src/main/java/com/example/exam/services/tika/LanguageDetectService.java
@@ -0,0 +1,11 @@
+package com.example.exam.services.tika;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LanguageDetectService {
+
+    public String identifyLanguage(String text) {
+        return "";
+    }
+}
