99ed6cd807a7d0b59db5c02558a92b9d4fe96544
hahak <skekdnl12@gmail.com>
2023-07-19 00:24:29 +0900

    api 서버 연결 컨피규레이션 작성 중

---
+++ api/src/main/java/my/application/api/services/member/MemberCRUDService.java
@@ -0,0 +1,23 @@
+package my.application.api.services.member;
+
+import lombok.RequiredArgsConstructor;
+import my.application.api.entities.mysql.MemberEntity;
+import my.application.api.repositories.mysql.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberCRUDService {
+    private final MemberRepository memberRepository;
+
+    public MemberEntity createMember(MemberEntity member) {
+        return memberRepository.save(member);
+    }
+
+    public List<MemberEntity> memberEntities() {
+        System.out.println("???");
+        return memberRepository.findAll();
+    }
+}
