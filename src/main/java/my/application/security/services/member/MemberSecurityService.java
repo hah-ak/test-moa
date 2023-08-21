81a603e188a463e2996cbdd8be4548ffed689e62
김용철 <skekdnl12@gmail.com>
2023-08-21 17:52:25 +0900

    oauth2 jwt 추가 중

--- src/main/java/my/application/security/services/member/MemberSecurityService.java
+++ src/main/java/my/application/security/services/member/MemberSecurityService.java
@@ -2,15 +2,11 @@ package my.application.security.services.member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import my.application.security.model.signIn.SignIn;
-import my.application.security.model.signUp.SignUp;
+import my.application.security.entities.signUp.SignUp;
 import my.domain.mysql.entities.MemberEntity;
 import my.domain.mysql.repositories.member.MemberRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Slf4j
 @Service
@@ -22,10 +18,10 @@ public class MemberSecurityService {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
         MemberEntity memberEntity = MemberEntity.builder()
-                .id(signUp.getId())
-                .password(bCryptPasswordEncoder.encode(signUp.getPassword()))
-                .name(signUp.getName())
-                .imageName(signUp.getImageName())
+                .id(signUp.id())
+                .password(bCryptPasswordEncoder.encode(signUp.password()))
+                .name(signUp.name())
+                .imageName(signUp.imageName())
                 .build();
         MemberEntity save = memberRepository.save(memberEntity);
         log.info("save memNo : {}",save.getMemNo());
--- src/main/java/my/application/security/services/member/MemberSignInUserDetails.java
+++ src/main/java/my/application/security/services/member/MemberSignInUserDetails.java
@@ -1,6 +1,5 @@
 package my.application.security.services.member;
 
-import lombok.Builder;
 import my.domain.mysql.entities.MemberEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
