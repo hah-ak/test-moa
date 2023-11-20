1d894d57938ad3a9c10153c78af0dc13b507e444
김용철 <skekdnl12@gmail.com>
2023-11-20 09:52:52 +0900

    git 연결 중

--- security/src/test/java/my/application/api/SecurityApplicationTests.java
+++ security/src/test/java/my/application/api/SecurityApplicationTests.java
@@ -1,11 +1,15 @@
 package my.application.api;
 
+import my.application.security.controllers.MainControllers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class SecurityApplicationTests {
-
     @Test
     void contextLoads() {
     }
