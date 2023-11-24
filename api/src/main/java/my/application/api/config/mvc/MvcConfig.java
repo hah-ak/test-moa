eba4e18ce56c82c7e45cde28ab9b0190617cdb74
김용철 <skekdnl12@gmail.com>
2023-11-24 17:00:21 +0900

    streaming 시도

--- api/src/main/java/my/application/api/config/mvc/MvcConfig.java
+++ api/src/main/java/my/application/api/config/mvc/MvcConfig.java
@@ -1,22 +1,35 @@
 package my.application.api.config.mvc;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import my.application.api.resolvers.UserTokenHeaderMethodArgumentResolver;
 import my.application.api.services.file.FileUtils;
+import my.application.security.common.DefaultHttpSecurity;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
+    private final DefaultHttpSecurity defaultHttpSecurity;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        defaultHttpSecurity.setDefaultSecurity(httpSecurity);
+        defaultHttpSecurity.setDefaultAuthorizationRequest(httpSecurity);
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        return httpSecurity.build();
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
--- libraries/security/src/main/java/my/application/security/config/SecurityConfig.java
+++ libraries/security/src/main/java/my/application/security/config/SecurityConfig.java
@@ -1,68 +1,27 @@
 package my.application.security.config;
 
+import com.google.api.client.http.HttpMediaType;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import my.application.security.filter.filter.MemberAuthenticationProcessingFilter;
-import my.application.security.filter.filter.MemberNullRepositoryAuthenticationFilter;
-import my.application.security.filter.handlers.CustomAccessDeniedHandler;
-import my.application.security.filter.handlers.MemberAuthenticationEntryPoint;
-import my.application.security.filter.manager.MemberAuthenticationProcessingProviderManager;
+import org.springframework.boot.context.FileEncodingApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
-import org.springframework.security.web.context.NullSecurityContextRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.awt.*;
 import java.util.List;
 
 @Slf4j
 @Configuration
 @PropertySource("classpath:security-${spring.profiles.active}.yaml")
-@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http,
-            MemberNullRepositoryAuthenticationFilter memberNullRepositoryAuthenticationFilter,
-            MemberAuthenticationProcessingFilter memberAuthenticationProcessingFilter
-    ) throws Exception {
-        // .addFilterAt(new MemberAuthenticationFilter("/**", new MemberAuthenticationProviderManager(memberAuthenticationProvider)), SecurityContextHolderFilter.class) 이렇게 하면 전지역에서 authenticate를 시도하는 형태가되어 전부다 authenticate성공으로 보고 리다이렉트를 돌게되어 무한루프발행.
-        // 리퀘스트가 생성되고 각 필터에서 그 상황에 맞는 필터링과정을 걸치고 이 과정에서 필요하다면 authentication 객체를 생성하고 그걸 활용해 authenticated인지 판단후 authorization을 authorization필터로 확인하는 과정임.
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .securityContext(securityContext->securityContext
-                        .securityContextRepository(new NullSecurityContextRepository())
-                )
-                // 일반적으로 authenticate를 동작하는 필터
-                .addFilterBefore(memberNullRepositoryAuthenticationFilter, AuthorizationFilter.class)
-                // entry point를 통해 들어온 경우에 만 동작하는 필터가 된다. 즉 authenticate 실패한경우에만 동작.
-                .addFilterBefore(memberAuthenticationProcessingFilter, MemberNullRepositoryAuthenticationFilter.class)
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(new CustomAccessDeniedHandler())
-                        .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
-                )
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/sign-in/**","/oauth2/**","/google/revoke","/google/login").permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/member/members/permit-all")).permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.build();
-    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
