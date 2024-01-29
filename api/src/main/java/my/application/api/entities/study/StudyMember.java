0aaa47bf750ee750c8e7033bead7e6b5707e3427
김용철 <skekdnl12@gmail.com>
2024-01-29 09:05:46 +0900

    study 자료 및 puml작성 연습

--- api/src/main/java/my/application/api/entities/study/StudyMember.java
+++ api/src/main/java/my/application/api/entities/study/StudyMember.java
@@ -1,19 +1,31 @@
 package my.application.api.entities.study;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.annotation.processing.Generated;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"name"},name = "studyUnique")}) // 컬럼에 unique쓰면 유니크 키에 랜덤한 이름이 부여됨. 그래서 여기에 쓰는게 좋다함
+//@SequenceGenerator(name = "study_seq_generator",sequenceName = "study_seq",initialValue = 1, allocationSize = 2)//시퀀스 지정
 public class StudyMember {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "study_seq_generator")
     private Long id;
+    @Column(updatable = false)
     private String name;
+    @Enumerated(EnumType.STRING)
+    private ROLE role;
+//    @Temporal(TemporalType.TIMESTAMP) local date time 같은거 쓰면 노필요.
+    private LocalDateTime createTIme;
+    @Lob // blob clob 타입
+    private String description;
+    @Transient
+    private String onlyMemoryInfo;
+
 }
--- api/src/main/java/my/application/api/services/study/JpaService.java
+++ api/src/main/java/my/application/api/services/study/JpaService.java
@@ -16,7 +16,6 @@ public class JpaService {
 
     @Qualifier("studyLocalContainerEntityManagerFactoryBean")
     private final EntityManagerFactory entityManagerFactory;
-
     public void first() {
 
 //        EntityManagerFactory hello = Persistence.createEntityManagerFactory("hello",properties);
@@ -30,9 +29,14 @@ public class JpaService {
             StudyMember jpaMember = new StudyMember();
             jpaMember.setId(1L);
             jpaMember.setName("test");
-            entityManager.persist(jpaMember); // persist context 에만 저장
+            entityManager.persist(jpaMember); // persist context 에만 저장  =>(db와 스프링 사이에 존재한다보면됨.)
             //persist context 의 장점
-            // 1차캐시 ===>
+            // 1차캐시 (셀렉트 혹은 내부에서 처음으로 만들어진 엔티티등 기록)===> 트랜잭션 내부에서 1차캐시가됨. 트랜잭션단위에서 같은값에 대해서 db에서 지속적 조회를 하지 않고 값을 가져옴.
+            // 엔티티 동일성 보장 ===> 같은 정보를 find해서 가져올때 완전히 같은 객체(해쉬값까지) 를 줌.
+            // 쓰기 지연 sql 저장소 ===> 최종적으로 커밋단계에서 그동안 만들어 놓은 쿼리를 실행.(save등 디비에 변화를 주는 jpa들의 쿼리)
+            // 자바 컬렉션같이 레퍼런스 값의 변화를 주면 그 자체가 업데이트 쿼리가 되어 persist가 필요 없음.  ==> commit 시점에 결국 모든변경(dirty checking)에 대해서 업데이트, 인서트를 해준단 소리
+            // flush를 직접호출하거나, jpql, commit 호출 시 db에 반영 ( jpql, commit내부에 flush가 있음)
+            // jpa에서 최종적으로 변경감지하고 상태를 기억하는 것은 flush후 1차 캐시에 있는 스냅샷들과 비교후 쓰기지연 저장소에 있는 쿼리들 수행함으로서 진행.
             transaction.commit(); // db에 직접적으로 실행하는 단계(쿼리실행)
         } catch (Exception e) {
             transaction.rollback();
